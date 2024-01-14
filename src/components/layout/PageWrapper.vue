45c9b41154f6653a2065ee962fc772e811556110
Rayden Li <79618253+lctrayden25@users.noreply.github.com>
2024-01-14 15:25:55 +0800

    update page-wrapper max width

--- src/components/layout/PageWrapper.vue
+++ src/components/layout/PageWrapper.vue
@@ -1,9 +1,10 @@
 <template>
-  <div class="relative w-full flex items-center flex-col justify-start min-h-screen">
-    <div class="max-w-6xl mx-auto h-full w-full p-5">
+  <div class="relative w-full flex items-center flex-col justify-start min-h-screen bg-[#EEF5FF]">
+    <div class="max-w-7xl mx-auto h-full w-full p-5">
       <NavBar />
-      <div class="pt-10">
-        <slot />
+      <div class="flex gap-10 pt-10">
+        <slot name="sidebar" />
+        <slot name="main" />
       </div>
     </div>
     <div class="w-full pt-10">
@@ -15,4 +16,12 @@
 <script lang="ts" setup>
 import Footer from './Footer.vue'
 import NavBar from './NavBar.vue'
+
+// const props = defineProps({
+//   isSidebar: {
+//     type: Boolean,
+//     required: false
+//   }
+// })
+// const { isSidebar } = props
 </script>
